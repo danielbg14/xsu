e7d37824dfbecd4743a1ba6a18aa219f618a6fda
unknown <stefanogabriela1@gmail.com>
2026-05-04 22:04:09 +0300

    Added FAQ bot

--- code-website/assets/js/main.js
+++ code-website/assets/js/main.js
@@ -3,8 +3,10 @@
  */
 import { initNav }    from './modules/nav.js';
 import { initReveal } from './modules/reveal.js';
+import { initFaqBot } from './modules/faq-bot.js';
 
 document.addEventListener('DOMContentLoaded', () => {
   initNav();
   initReveal();
+  initFaqBot();
 });
